25206a47f710328e0b6f3f84a91385bb238b5a97
sbx_user1060 <sbx_user1060@ip-10-128-67-157.eu-west-2.compute.internal>
2019-01-25 16:02:58 +0000

    Fri Jan 25 16:02:58 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -27,15 +27,17 @@ def checkout(skus):
     for product, quantity in counts.iteritems():
         if product in PRICES:
             while quantity > 0:
-                print(product, quantity)
+                #print("quantity", product, quantity)
                 for price_quantity in PRICE_QUANTITIES[product]:
-                    print(product, price_quantity)
+                    #print("price quantity", product, price_quantity)
                     if price_quantity <= quantity:
                         quantity -= price_quantity
                         total += PRICES[product][price_quantity]
+                        break
         else:
             return -1
 
     return total
 
 print(checkout("AAAAAA"))
+print(checkout("BBBB"))
