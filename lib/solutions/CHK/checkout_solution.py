a6e5bd7f10c9d521f979007cc06f2dfc6932f364
sbx_user1060 <sbx_user1060@ip-10-128-67-157.eu-west-2.compute.internal>
2019-01-25 15:16:13 +0000

    Fri Jan 25 15:16:13 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,7 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    if type(skus) not str:
+
+
     raise NotImplementedError()
--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -4,5 +4,3 @@
 # friend_name = unicode string
 def hello(friend_name):
     return "Hello, {0}!".format(friend_name)
-
-
