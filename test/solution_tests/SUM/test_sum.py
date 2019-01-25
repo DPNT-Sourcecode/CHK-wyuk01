c5bce8a4c1c4edd7faea1f681dd57f91e87bac8a
sbx_user1060 <sbx_user1060@ip-10-128-67-157.eu-west-2.compute.internal>
2019-01-25 14:59:23 +0000

    Fri Jan 25 14:59:23 UTC 2019

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -2,7 +2,11 @@ from solutions.SUM import sum_solution
 import unittest
 
 class TestSum(unittest.TestCase):
-    def test_sum(self):
-        assert sum_solution.compute(1, 2) == 3
-        self.assertRaises(TypeError, sum_solution.compute("2", 1))
+    def test_sum_correct(self):
+        self.assertEqual(sum_solution.compute(1, 2), 3)
+
+    def test_sum_incorrect_type(self):
+        self.assertRaises(TypeError, sum_solution.compute, "2", 1)
+        self.assertRaises(TypeError, sum_solution.compute, "2", "2")
+
 
