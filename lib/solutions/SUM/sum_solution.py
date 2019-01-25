5154fd13cb22096ec647d8e6a3cf72dc3eaef1e9
sbx_user1060 <sbx_user1060@ip-10-128-67-157.eu-west-2.compute.internal>
2019-01-25 14:53:23 +0000

    Fri Jan 25 14:53:23 UTC 2019

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,5 +1,10 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    if type(x) is not 
+    if type(x) is not int or type(y) is not int:
+        raise TypeError("x and y should be integers between 0 and 100")
+
+    if x < 0 or x > 100 or y < 0 or y > 100:
+        raise ValueError("x and y should be between 0 and 100")
+
     return x + y;
 
