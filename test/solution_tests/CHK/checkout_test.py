00c5c9275acdbe1b062dc0af4a9b5776fc93d2eb
sbx_user1060 <sbx_user1060@ip-10-128-67-157.eu-west-2.compute.internal>
2019-01-25 16:49:46 +0000

    Fri Jan 25 16:49:46 UTC 2019

--- test/solution_tests/CHK/checkout_test.py
+++ test/solution_tests/CHK/checkout_test.py
@@ -13,4 +13,8 @@ class TestSum(unittest.TestCase):
             self.assertEqual(checkout_solution.checkout(product), price_quantities[1])
 
     def test_valid_remover_correctly_removes(self):
+        for remover_product, (quantity_needed, product_to_remove, quantity_to_remove) \
+                in checkout_solution.REMOVERS.iteritems():
+            input = remover_product * quantity_needed
+
         self.assertEqual(checkout_solution.checkout("EEB"), checkout_solution.checkout("EE"))
