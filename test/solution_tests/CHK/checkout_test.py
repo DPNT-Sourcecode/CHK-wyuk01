ef21bc8007497d091bbb26760a104db132daaf32
sbx_user1060 <sbx_user1060@ip-10-128-67-157.eu-west-2.compute.internal>
2019-01-25 15:56:48 +0000

    Fri Jan 25 15:56:48 UTC 2019

--- test/solution_tests/CHK/checkout_test.py
+++ test/solution_tests/CHK/checkout_test.py
@@ -8,16 +8,6 @@ class TestSum(unittest.TestCase):
     def test_invalid_sku_gives_minus_one(self):
         self.assertEqual(checkout_solution.checkout("F"), -1)
 
-    def test_sum_correct(self):
-        self.assertEqual(sum_solution.compute(1, 2), 3)
-        self.assertEqual(sum_solution.compute(0, 2), 2)
-        self.assertEqual(sum_solution.compute(0, 100), 100)
-
-    def test_sum_incorrect_type_raises_type_error(self):
-        self.assertRaises(TypeError, sum_solution.compute, "2", 1)
-        self.assertRaises(TypeError, sum_solution.compute, "2", "2")
-
-    def test_sum_incorrect_value_raises_value_error(self):
-        self.assertRaises(ValueError, sum_solution.compute, -1, -1)
-        self.assertRaises(ValueError, sum_solution.compute, 20, -1)
-        self.assertRaises(ValueError, sum_solution.compute, 100, 200)
+    def test_valid_sku_gives_correct_price(self):
+        for product, price_quantities in checkout_solution.PRICES.iteritems():
+            self.assertEqual(checkout_solution.checkout(product), price_quantities[1])
