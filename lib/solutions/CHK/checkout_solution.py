cc124171fdf360697a3cbf9708ba96e348da1b19
sbx_user1060 <sbx_user1060@ip-10-128-67-157.eu-west-2.compute.internal>
2019-01-25 15:25:13 +0000

    Fri Jan 25 15:25:13 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,9 +1,9 @@
-
+from collections import Counter
 
 PRICES = {"A": {1: 50, 3: 130},
           "B": {1: 30, 2: 35},
           "C": {1: 20},
-          "D":, {}}
+          "D": {1: 15}}
 
 # noinspection PyUnusedLocal
 # skus = unicode string
@@ -11,7 +11,13 @@ def checkout(skus):
     if not isinstance(skus, basestring):
         return -1
 
+    counts = Counter(skus)
 
-    raise NotImplementedError()
+    print(counts)
+
+
+checkout("ABCDEFAB")
+
+
 
 
